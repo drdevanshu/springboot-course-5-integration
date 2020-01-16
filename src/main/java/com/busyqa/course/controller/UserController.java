7ffe4628de6900b38d5d06cc124c4463279dd4a7
Carlos Zegarra <cgzegarra@gmail.com>
2020-01-16 01:50:39 -0500

    Adjustment

--- src/main/java/com/busyqa/course/controller/UserController.java
+++ src/main/java/com/busyqa/course/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.busyqa.course.jpa.User;
 import com.busyqa.course.service.UserService;
 
 @RestController
+@CrossOrigin
 public class UserController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
